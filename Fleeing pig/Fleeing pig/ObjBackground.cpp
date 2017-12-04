0e1e9112897d8a3ddbc59f5876b09304500ed210
student <student@DEV2-05>
2017-12-04 11:23:41 +0900

    a

---
+++ Fleeing pig/Fleeing pig/ObjBackground.cpp
@@ -0,0 +1,26 @@
+//使用するヘッダーファイル
+#include "GameL\DrawTexture.h"
+#include "GameHead.h"
+
+#include "ObjBackground.h"
+
+//使用するネームスペース
+using namespace GameL;
+
+//イニシャライズ
+void CObjBackground::Init()
+{
+
+
+}
+
+//アクション
+void CObjBackground::Action()
+{
+
+}
+//ドロー
+void CObjBackground::Draw()
+{
+
+}
